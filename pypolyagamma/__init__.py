809b2c22a1882e288f2d664ad7b756b029a5fbf7
Scott Linderman <scott.linderman@gmail.com>
2016-01-27 17:32:17 -0500

    python 3 safe imports?

--- pypolyagamma/__init__.py
+++ pypolyagamma/__init__.py
@@ -1,1 +1,2 @@
-from pypolyagamma import PyRNG, PyPolyaGamma, pgdrawvpar, get_omp_num_threads
+from __future__ import absolute_import
+from pypolyagamma.pypolyagamma import PyRNG, PyPolyaGamma, pgdrawvpar, get_omp_num_threads
